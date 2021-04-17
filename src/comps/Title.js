6f9be1957f875bebaeb96b117347e55923bca502
AurobindoGupta <aurogpt10@gmail.com>
2021-04-17 10:32:54 +0530

    bla

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
     <div className="title">
       <h1>BlaGram</h1>
       <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>Add some pictures, lets have some fun.</p>
     </div>
   )
 }
